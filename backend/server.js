def9500a6aa7780c17aa525104b1d2a8755a701a
CalebAnderson-Coder <bandersonlopez27.ba@gmail.com>
2025-08-11 13:42:50 -0400

    Prepare for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*' // Permitir cualquier origen para el despliegue
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
